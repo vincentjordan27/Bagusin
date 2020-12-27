a8a3ae7ec339c04811994f037f9f31ca67c5ed73
apridos <apridos@yandex.com>
2020-12-27 16:40:50 +0700

    perbaikan navbar utk akun mekanik

--- bagusin/resources/views/includes/navbar.blade.php
+++ bagusin/resources/views/includes/navbar.blade.php
@@ -3,39 +3,54 @@
   <!-- Navbar content -->
   <a class="navbar-brand" href="/"><img id="logo" src="/images/bagusin.png"></a>
   <div style="padding-right:50px;text-align: right; margin-top:2vh">
-    @if(!Auth::guard('admin')->user())
-    <a class="btn nav-item" data-toggle="modal" data-target="#aboutmodal">Tentang Kami</a>
-    <a class="nav-item" href="/myorder"><img id="bell" src="/images/bell.png"></a>
 
-
-    @if ((!Auth::guard('customer')->user()) and (!Auth::guard('mechanic')->user()))
+    @if ((!Auth::guard('customer')->user()) and (!Auth::guard('mechanic')->user()) and (!Auth::guard('admin')->user()))
     <a class="btn nav-item" href="/daftar">Daftar</a>
     <a class="btn nav-item" href="/masuk">Masuk</a>
     @endif
+
+    <!-- Admin -->
+    @if(!Auth::guard('admin')->user())
+    <a class="btn nav-item" data-toggle="modal" data-target="#aboutmodal">Tentang Kami</a>
+    <a class="nav-item" href="/myorder"><img id="bell" src="/images/bell.png"></a>
+    @endif
+
+    <!-- Customer -->
     @if (Auth::guard('customer')->user())
     <a class="btn nav-item" type="button" class="" data-toggle="modal" data-target="#profileModal">
       {{ Auth::guard('customer')->user()->name }}
     </a>
-    @endif
-
-
-
-    <!-- Profile Modal -->
-    </div>
-    @if (Auth::guard('customer')->user() or Auth::guard('mechanic')->user())
-        <div class="modal fade" id="profileModal" tabindex="-1" role="dialog" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content" style="width: 180px; margin-left:50vw">
-              <div class="modal-body">
-                <a href="/akun" class="profilemenu">Pengaturan Akun</a><br>
-                <a href="/riwayat" class="profilemenu">Pesanan Saya</a><br>
-                <a href="/keluar" class="profilemenu">Keluar</a><br>
-              </div>
-            </div>
+    <div class="modal fade" id="profileModal" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content" style="width: 180px; margin-left:50vw">
+          <div class="modal-body">
+            <a href="/akun" class="profilemenu">Pengaturan Akun</a><br>
+            <a href="/riwayat" class="profilemenu">Pesanan Saya</a><br>
+            <a href="/keluar" class="profilemenu">Keluar</a><br>
           </div>
         </div>
+      </div>
+    </div>
     @endif
-    @else
+
+    <!-- Mechanic -->
+    @if (Auth::guard('mechanic')->user())
+    <a class="btn nav-item" type="button" class="" data-toggle="modal" data-target="#profileModal">
+      {{ Auth::guard('mechanic')->user()->name }}
+    </a>
+    <div class="modal fade" id="profileModal" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content" style="width: 180px; margin-left:50vw">
+          <div class="modal-body">
+            <a href="/akun" class="profilemenu">Pengaturan Akun</a><br>
+            <a href="/riwayat" class="profilemenu">Pesanan Saya</a><br>
+            <a href="/keluar" class="profilemenu">Keluar</a><br>
+          </div>
+        </div>
+      </div>
+    </div>
+    @endif
+
     @if (Auth::guard('mechanic')->user())
     <a class="btn nav-item" type="button" class="" data-toggle="modal" data-target="#profileModal">
       {{ Auth::guard('mechanic')->user()->name }}
@@ -44,7 +59,7 @@
     <a class="btn nav-item" href="/admin/dashboard">Admin</a>
     <a class="btn nav-item" href="/admin/logout">Keluar</a>
     @endif
-    @endif
+
     <!-- End Profile Modal -->
 </nav>
 <!--About us modal -->
